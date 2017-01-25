ac4a17d4b95e21e61e01f4e7f063714d5a0ed2bd
Shaun Milby <smilby0247@my.sctd.edu>
2017-01-24 19:29:09 -0500

    Yep changes from car to animal
    cool

---
+++ Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions_SCM
+{
+    class Animal
+    {
+        public string Type { get; set; }
+
+
+
+
+
+
+
+        public Animal(string type)
+        {
+            this.Type = type;
+        }
+
+        public string Describe()
+        {
+            return "This animal is a " + this.Type;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions_SCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Animal("Zebra"));
+            animals.Add(new Animal("Giraffe"));
+            animals.Add(new Animal("Hippo"));
+            animals.Add(new Animal("Lion"));
+            animals.Add(new Animal("Elephant"));
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.Describe());
+            }
+
+
+            Console.ReadKey();
+           
+
+
+        }
+    }
+}
+       
